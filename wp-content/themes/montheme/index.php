bdb529bfc7cb186fde5d11f497e636489b4e0fda
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-05-10 14:54:46 +0200

    boucle

--- wp-content/themes/montheme/index.php
+++ wp-content/themes/montheme/index.php
@@ -1,6 +1,15 @@
 <?php get_header() ?>
-<br>
-Bonjour tout le monde!
-<br>
-<?php wp_title(); ?>
+<!-- <br>
+Bonjour tout le monde! : <?php wp_title(); ?> -->
+
+<?php if (have_posts()) : ?>
+    <ul>
+        <?php while (have_posts()) : the_post(); ?>
+            <li><?php the_title() ?></li>
+        <?php endwhile; ?>
+    </ul>
+<?php else : ?>
+    <h1>Pas d'articles</h1>
+<?php endif; ?>
+
 <?php get_footer() ?>
