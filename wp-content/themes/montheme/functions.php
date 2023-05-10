fa93a5225761f075bb28e682e0a728c6cf5b5dc4
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-05-10 14:28:57 +0200

    test action

--- wp-content/themes/montheme/functions.php
+++ wp-content/themes/montheme/functions.php
@@ -1,2 +1,14 @@
 <?php
+
+function montheme_register_assets()
+{
+    wp_register_style('tailwind', 'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css');
+    wp_enqueue_style('tailwind');
+};
+
+add_action('wp_enqueue_scripts', 'montheme_register_assets');
+add_action('wp_head', function () {
+    echo "test add_action header";
+});
+
 add_theme_support('title-tag');
